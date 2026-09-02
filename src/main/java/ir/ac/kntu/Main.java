9354f9ff5f3d75874b1135152c557078791c4bee
Mohammad Afra <mohammadkhaje04@gmail.com>
2026-09-03 01:26:52 +0330

    feat(cli): add Main application entrypoint bootstrapping FQL engine

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -1,2 +1,20 @@
-package ir.ac.kntu;public class Main {
+package ir.ac.kntu;
+
+import ir.ac.kntu.cli.ReplEngine;
+import ir.ac.kntu.engine.DatabaseEngine;
+import ir.ac.kntu.model.Database;
+import ir.ac.kntu.parser.CommandParser;
+
+/**
+ * Main application entry point bootstrapping the FQL engine and REPL session.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Database database = new Database();
+        DatabaseEngine engine = new DatabaseEngine(database);
+        CommandParser parser = new CommandParser();
+
+        ReplEngine replEngine = new ReplEngine(engine, parser);
+        replEngine.start();
+    }
 }
